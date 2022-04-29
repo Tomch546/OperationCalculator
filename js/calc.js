795ea4145cf1f5f401551b2af5755c22d50f8f49
Tommy <tommych123@gmail.com>
2022-04-29 15:04:27 -0400

    modified structure and added more expressions

--- js/calc.js
+++ js/calc.js
@@ -9,39 +9,53 @@ function update_input() {
 	input2 = parseInt(document.getElementById("inputB").value);
 	add(input1, input2);
 	subtract(input1, input2);
+	multiply(input1, input2);
+	divide(input1, input2);
+	modulo(input1, input2);
+	square(input1);
+	exponent(input1, input2);
+	root(input1);
 }
 
 // BASIC
 function add(inputA, inputB) {
-	document.getElementById("Addition").innerHTML = "Addition: " + input1 + " + " + input2 + " = " + (inputA + inputB);
+	document.getElementById("Addition").innerHTML = input1 + " + " + input2;
+	document.getElementById("Additionresult").innerHTML = inputA + inputB;
 }
 
 function subtract(inputA, inputB) {
-	document.getElementById("Subtraction").innerHTML = "Subtraction: " + input1 + " - " + input2 + " = " + (inputA - inputB);
+	document.getElementById("Subtraction").innerHTML = input1 + " - " + input2;
+	document.getElementById("Subtractionresult").innerHTML = inputA - inputB;
 }
 
 function multiply(inputA, inputB) {
-	return inputA * inputB;
+	document.getElementById("Multiplication").innerHTML = input1 + " * " + input2;
+	document.getElementById("Multiplicationresult").innerHTML = inputA * inputB;
 }
 
 function divide(inputA, inputB) {
-	return inputA / inputB;
+	document.getElementById("Division").innerHTML = input1 + " / " + input2;
+	document.getElementById("Divisionresult").innerHTML = inputA / inputB;
 }
 
-function modulus(inputA, inputB) {
-	return inputA % inputB;
+function modulo(inputA, inputB) {
+	document.getElementById("Modulo").innerHTML = input1 + " % " + input2;
+	document.getElementById("Moduloresult").innerHTML = inputA % inputB;
 }
 
-function square(input) {
-	return input ** 2;
+function square(inputA) {
+	document.getElementById("Square").innerHTML = input1 + "^2 ";
+	document.getElementById("Squareresult").innerHTML = inputA ** 2;
 }
 
 function exponent(inputA, inputB) {
-	return inputA ** inputB;
+	document.getElementById("Exponent").innerHTML = input1 + "^" + input2;
+	document.getElementById("Exponentresult").innerHTML = inputA ** inputB;
 }
 
-function root(input) {
-	return Math.sqrt(input); 
+function root(inputA) {
+	document.getElementById("Root").innerHTML = "√" + input1;
+	document.getElementById("Rootresult").innerHTML = Math.sqrt(inputA);
 }
 
 // maybe no go
